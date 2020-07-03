fa75d2035d21b90ae627812c020da65f7b4dc5a5
Lee <sunghelee@lm-sel-16517094.corp.ebay.com>
2020-07-03 13:55:10 +0900

    @Prop으로 데이터 전달

---
+++ README.md
@@ -0,0 +1,27 @@
+# Todo
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+> 빌드를 하게 되면 `dist`폴더가 생기게 되는데 이 폴더를 나중에 배포하게 됨
+
+`public/` : 정적 파일
+`asset/` : css, 이미지들
+`components/` : 모든 페이지에서 사용하는 공통적인 컴포넌트
+`views` : 각 화면의 컴포넌트
+
+
+- @Decorator
+
+  - 
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Message></Message>
+    <children :parentMessage="message"></children>
+  </div>
+</template>
+
+<script lang="ts">
+// @ is an alias to /src
+import {Component, Vue} from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue';
+import Message from '@/components/message';
+import children from '@/components/children';
+
+@Component({
+  components: {
+    Message,
+    children,
+  },
+})
+
+export default class Home extends Vue {
+  public message: string = 'send message';
+}
+</script>
